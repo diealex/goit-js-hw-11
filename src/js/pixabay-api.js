ce87a7d4dbcf064b1dc65f191473feb70c9b716b
diealex <serendipisland@gmail.com>
2025-03-27 22:12:09 +0100

    chatching errors behaviour added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import renderFnc from './render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const myApiKey = '49502149-61d0264429aca11602d0077d5';
 const serverRequest = searchWord => {
@@ -13,7 +15,14 @@ const serverRequest = searchWord => {
       renderFnc(response);
     })
     .catch(error => {
-      console.log(error);
+      document.querySelector('.loader').style.display = 'none';
+      document.querySelector('.gallery').innerHTML = '';
+      iziToast.error({
+        message: `${error}`,
+        backgroundColor: '#EF4040',
+        close: true,
+        position: 'topRight',
+      });
     });
 };
 
